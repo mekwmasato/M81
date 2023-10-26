7b9ffe2b1c00ea519978c8b527c066a5cdff2855
mekwmasato0813 <massun10359@gmail.com>
2023-10-26 12:53:13 +0900

    a

--- M81-actuator.py
+++ M81-actuator.py
@@ -1,14 +1,12 @@
 #M81をアクチュエーターに合わせて結果を保存するためのプログラム。使い方は開始、終了地点を入力してヨーイドンで実行するだけ
+#pip install lakeshoreでlakeshoreインストール必須。多分keyboardあたりがwindowsでしか動かないかも
 from lakeshore import SSMSystem #M81のこと
 from time import sleep
 from math import sqrt
-import csv
-import datetime
-import os
-import keyboard
+import csv,datetime,os,keyboard
 
 M81 = SSMSystem() #初期値ではUSBから接続、ip指定でTCPで接続
-#M81=SSMSystem.connect_tcp('10.10.10.13',7777, 100) #多分これでネットワークから接続する
+#M81=SSMSystem.connect_tcp('10.10.10.13',7777, 100) #多分これでethernetから接続する
 
 S1 = M81.get_source_module(1) #ソースモジュール
 M1 = M81.get_measure_module(1) #計測モジュール
@@ -32,25 +30,28 @@ distance = 50
 #csv setting
 now = datetime.datetime.today()
 hourstr = "_" + now.strftime("%H%M%S")
-filename = "record_" + now.strftime("%Y%m%d") + hourstr + ".csv"
+filename = "M81-actuator_" + now.strftime("%Y%m%d") + hourstr + ".csv"
 
 try:
-    os.makedirs('record',exist_ok=True)
-    #create CSV
-    with open(os.path.join('record',filename), 'a', newline= '') as f:
+    os.makedirs('M81-actuator-records',exist_ok=True) #open dir
+    with open(os.path.join('M81-actuator-records',filename), 'a', newline= '') as f: #create CSV file
         writer = csv.writer(f)
         writer.writerow(["距離[mm]","R[V]","theta[Θ]"])
-
+    S1.enable() #S1(電流モジュール)を起動
+    
+    A = float(input("開始地点を入力してください[mm]:"))
+    B = float(input("終了地点を入力してください[mm]:"))
+    speed = float(input("速度を入力してください:"))#速度はmm/sで欲しいけどどうなってるかわからん
+    space = float(input("測定したい間隔[mm]を入力してください:"))
+    
     while True:
-
-        S1.enable() #S1を起動
 
         sleep(1.5)
 
-        lock_in_magnitude = M1.get_lock_in_r() #LockinモードのRを取得
-        print(lock_in_magnitude)
-        lock_in_theta = M1.get_lock_in_theta() 
-        print(lock_in_theta)
+        lock_in_magnitude = M1.get_lock_in_r()#LockinモードのRを取得
+        lock_in_theta = M1.get_lock_in_theta()
+        print(f"lock_in_magnitude:{lock_in_magnitude}")
+        print(f"lock_in_theta:{lock_in_theta}")
 
         #enterが押されるごとにcsvに書き込む
         if keyboard.is_pressed("enter"):
@@ -62,4 +63,8 @@ try:
 except KeyboardInterrupt:
     S1.disable() #S1を停止
     f.close
+    pass
+
+except Exception as e:
+    print(e)
     pass
