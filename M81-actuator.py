588efb810ec73f425be2adaaf33c70dce28400f5
MasatoMaekawa <massun10359@gmail.com>
2023-10-26 15:31:09 +0900

    actuatorあぷり

--- M81-actuator.py
+++ M81-actuator.py
@@ -3,8 +3,9 @@
 from lakeshore import SSMSystem #M81のこと
 from time import sleep
 from math import sqrt
-import csv,datetime,os,keyboard
+import csv,datetime,os,keyboard, time
 
+print("Connect M81")
 M81 = SSMSystem() #初期値ではUSBから接続、ip指定でTCPで接続
 #M81=SSMSystem.connect_tcp('10.10.10.13',7777, 100) #多分これでethernetから接続する
 
@@ -24,47 +25,74 @@ S1.use_ac_coupling() #カップリングをACに設定
 
 M1.setup_lock_in_measurement('S1', 0.1) #S1の周波数を参照信号にし、timeconstantを100msに設定
 
-#端からの距離
-distance = 50
-
-#csv setting
-now = datetime.datetime.today()
-hourstr = "_" + now.strftime("%H%M%S")
-filename = "M81-actuator_" + now.strftime("%Y%m%d") + hourstr + ".csv"
 
 try:
     os.makedirs('M81-actuator-records',exist_ok=True) #open dir
-    with open(os.path.join('M81-actuator-records',filename), 'a', newline= '') as f: #create CSV file
-        writer = csv.writer(f)
-        writer.writerow(["距離[mm]","R[V]","theta[Θ]"])
+    # CSVファイル設定
+    now = datetime.datetime.today()
+    hourstr = "_" + now.strftime("%H%M%S")
+    filename = "M81-actuator_" + now.strftime("%Y%m%d") + hourstr + ".csv"
+    #filepath = os.path.join('M81-actuator-records', filename)
+    
+    print("測定は**モードを使用して位置と速度を決定してください")
+    start_point = float(input("開始地点を入力してください[mm]:"))
+    end_point = float(input("終了地点を入力してください[mm]:"))
+    speed = float(input("移動速度を入力してください[mm/s]:"))
+    measure_interval_distance = float(input("測定したい間隔[mm]を入力してください:"))
+    
+    total_distance = end_point - start_point
+    time_per_interval = measure_interval_distance / speed #各測定間隔の時間
+    number_of_measurements = int(total_distance / measure_interval_distance) + 1 #測定回数(終了点も含むためプラス１)
+    
+    
     S1.enable() #S1(電流モジュール)を起動
+    sleep(1)
+    print("S1モジュール起動しました")
     
-    A = float(input("開始地点を入力してください[mm]:"))
-    B = float(input("終了地点を入力してください[mm]:"))
-    speed = float(input("速度を入力してください:"))#速度はmm/sで欲しいけどどうなってるかわからん
-    space = float(input("測定したい間隔[mm]を入力してください:"))
-    
-    while True:
-
-        sleep(1.5)
-
-        lock_in_magnitude = M1.get_lock_in_r()#LockinモードのRを取得
-        lock_in_theta = M1.get_lock_in_theta()
-        print(f"lock_in_magnitude:{lock_in_magnitude}")
-        print(f"lock_in_theta:{lock_in_theta}")
-
-        #enterが押されるごとにcsvに書き込む
-        if keyboard.is_pressed("enter"):
-            print(f"距離:{distance}mm R:{lock_in_magnitude:.7f} theta:{lock_in_theta:.7f}")
-            data = [distance,lock_in_magnitude,lock_in_theta]
+    with open(os.path.join("M81-actuator-records",filename), 'w', newline='') as f: #create CSV file
+        writer = csv.writer(f)
+        writer.writerow(["測定地点[mm]","R[V]","theta[Θ]"]) #最初の一行に説明を書き込む
+        
+        start_time = time.time()
+        next_measurement_time = start_time
+        
+        for i in range(number_of_measurements):
+            current_time = time.time()
+            while current_time < next_measurement_time:
+                time.sleep(next_measurement_time - current_time)  # 次の測定時刻まで待機
+                current_time = time.time()
+            
+            current_position = start_point + (i * measure_interval_distance)# 現在の測定位置を計算
+            #ロックインアンプデータ取得
+            lock_in_magnitude = M1.get_lock_in_r()#LockinモードのRを取得
+            lock_in_theta = M1.get_lock_in_theta()
+            print(f"lock_in_magnitude:{lock_in_magnitude}")
+            print(f"lock_in_theta:{lock_in_theta}")
+            
+            data = [current_position, lock_in_magnitude, lock_in_theta]
             writer.writerow(data)
-            distance += 2
+            
+            next_measurement_time += time_per_interval  #次の測定時間を設定
+            
+        # 最後のポイントが正確に終了地点でない場合、最後の測定を実施
+        if current_position < end_point:
+            #ロックインアンプデータ取得
+            lock_in_magnitude = M1.get_lock_in_r()#LockinモードのRを取得
+            lock_in_theta = M1.get_lock_in_theta()
+            print(f"lock_in_magnitude:{lock_in_magnitude}")
+            print(f"lock_in_theta:{lock_in_theta}")
+            data = [current_position, lock_in_magnitude, lock_in_theta]
+            writer.writerow(data)
+        
 
+# キーボード割り込みが発生した場合
 except KeyboardInterrupt:
-    S1.disable() #S1を停止
-    f.close
-    pass
+    print("ユーザーによってプログラムが中断されました。")
+    S1.disable()
 
 except Exception as e:
     print(e)
-    pass
+
+finally:
+    S1.disable()
+    f.close()
